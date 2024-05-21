c7e2df8f184024a7cafaf142e69d57cda8b6393a
github-srijoyee <137808302+github-srijoyee@users.noreply.github.com>
2024-05-21 22:18:47 +0530

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import './App.css'
+import './App.css';
 import React from 'react';
 import MagicWandCursor from './components/MagicWandCursor';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
-import AnimatedCursor from "react-animated-cursor";
-import SwirlCursor from './components/SwirlCursor';
+import AnimatedCursor from 'react-animated-cursor';
 
 const App = () => {
   return (
     <div>
-<AnimatedCursor/>
-      <NavBar/>
-      <Hero/>
-      <Footer/>
+     
+      <NavBar />
+      <Hero />
+      <Footer />
+      <MagicWandCursor
+       />
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        color="255, 255, 255"
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={2}
+      />
     </div>
   );
 };
